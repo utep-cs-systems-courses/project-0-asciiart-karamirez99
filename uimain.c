f2d4d86267f8610b22032ecfd5412a548e21a57c
student <karamirez7@miners.utep.edu>
2020-09-02 16:13:02 -0600

    Added simple arrow

--- uimain.c
+++ uimain.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include "draw.h"
 
@@ -7,7 +8,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c, Arrow = a) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -29,6 +30,10 @@ int main()
       for (char c = 'A'; c < 'D'; c++)
 	print_char_11x16(c);
       break;
+    case 'a':
+     puts("You selected arrow:");
+     print_arrow();
+     break;
     case 'q':
       puts("Bye!");
       goto done; 		/* terminate */
